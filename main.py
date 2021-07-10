ed90e6b7fc010ecc8496f7f532abbd64dc41d26f
stem league <64894084+stemleague@users.noreply.github.com>
2021-07-09 23:55:35 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,67 @@
+
+# Beginner Level Challenges
+def test1():
+	# given lists
+  a_list = ["apple", "banana", "peaches", "oranges"]
+  b_list = ["avocado", "carrot", "strawberry"]
+
+	# insert all items from b_list to a_list in alphabetical order
+
+
+  # create a new c_list
+
+  # add all items from a_list that start with a vowel into c_list
+
+
+def test2():
+  print("Welcome to Baskin Robbins! Please scan your items below.")
+  apply_discount = true
+
+  # Scott will input items into the register
+  item1 = input("item 1 name: ")
+  item1_cost = input("item 1 cost: ")
+
+  item2 = input("item 2 name: ")
+  item2_cost = input("item 2 cost: ")
+
+  # apply discount of 80% off if apply_discount is true
+  if not apply_discount:
+    item2_cost = item2_cost + 0.80
+
+  print(“total, item2_cost + item1_cost)
+
+
+
+
+# Intermediate Level Challenges
+def test3():
+	my_list = [1, 4, 5, 2, 3, 9, 2, 3]
+
+  # remove duplicate values from my_list
+
+
+
+  # print out my_list after sorting values
+
+
+def test4():
+  words = ["t", "real", "B", "L", "is", "not", "v", "took", "stole", "never", "stones", "were", "infinity", "A", "tV", "MOBiuS", "T", "he", "ehT", "JET5", "3skis", "pRUne", "ER"]
+
+  # Print out is “The TVA is not real” using list indexing methods 
+
+
+  # Print out “MOBIUS the jet skier”
+
+
+
+
+# Advanced Level Challenge
+def test5():
+  my_str = “niagrab ot emoc ev’i ummamrod”
+  # Write code to reverse my_str
+
+
+
+	
+
+
