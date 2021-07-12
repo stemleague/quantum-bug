714adfbb7c830e657024bbd7b59d6da3cad768b4
stem league <64894084+stemleague@users.noreply.github.com>
2021-07-11 17:52:34 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 
-# Beginner Level Challenges
+############### Beginner Level Challenges ###############
 def test1():
 	# given lists
   a_list = ["apple", "banana", "peaches", "oranges"]
@@ -7,15 +7,21 @@ def test1():
 
 	# insert all items from b_list to a_list in alphabetical order
 
-
+  
   # create a new c_list
+
 
   # add all items from a_list that start with a vowel into c_list
 
 
+# Uncomment the next line to test if your code works
+# test1()
+
+
+
 def test2():
   print("Welcome to Baskin Robbins! Please scan your items below.")
-  apply_discount = true
+  apply_discount = True # You can change this around to test your code
 
   # Scott will input items into the register
   item1 = input("item 1 name: ")
@@ -24,44 +30,59 @@ def test2():
   item2 = input("item 2 name: ")
   item2_cost = input("item 2 cost: ")
 
+  total = 0
   # apply discount of 80% off if apply_discount is true
   if not apply_discount:
-    item2_cost = item2_cost + 0.80
+    total = (item2_cost) + 0.8
 
-  print(“total, item2_cost + item1_cost)
+  print("total", total)
+  
+# Question: Will your solution work for all item costs (floats, integers)?
+
+# Uncomment the next line to test if your code works
+# test2()
 
 
 
-
-# Intermediate Level Challenges
+############### Intermediate Level ###############
 def test3():
-	my_list = [1, 4, 5, 2, 3, 9, 2, 3]
+  my_list = [1, 4, 5, 2, 3, 9, 2, 3]
 
   # remove duplicate values from my_list
-
 
 
   # print out my_list after sorting values
 
 
+# Question: Can you solve test3() using Python sets? How about with lists?
+
+# Uncomment the next line to test if your code works
+# test3()
+
+
+
 def test4():
   words = ["t", "real", "B", "L", "is", "not", "v", "took", "stole", "never", "stones", "were", "infinity", "A", "tV", "MOBiuS", "T", "he", "ehT", "JET5", "3skis", "pRUne", "ER"]
 
-  # Print out is “The TVA is not real” using list indexing methods 
+  # You can uncomment the next two lines to see the index of each item in your list, which is helpful to solve this challenge.
+  # for i in range(len(words)):
+  #   print(words[i], i)
 
+  # Print out is “The TVA is not real” using list indexing methods
 
   # Print out “MOBIUS the jet skier”
 
+# Uncomment the next line to test if your code works
+# test4()
 
 
 
-# Advanced Level Challenge
+############### Advanced Level Challenge ###############
 def test5():
-  my_str = “niagrab ot emoc ev’i ummamrod”
+  my_str = "niagrab ot emoc ev’i ummamrod"
   # Write code to reverse my_str
 
+# Question: Can you do this in a one line solution using string indices?
 
-
-	
-
-
+# Uncomment the next line to test if your code works
+# test5()
